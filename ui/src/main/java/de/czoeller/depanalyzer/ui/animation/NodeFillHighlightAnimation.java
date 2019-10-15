f7aac345fef939d1c0ee1e1f7c166deef28e98c0
Christian Zöller <noex_home@yahoo.de>
2019-10-15 23:46:12 +0200

    Fixed bug where state of searched nodes was not restored

--- ui/src/main/java/de/czoeller/depanalyzer/ui/animation/NodeFillHighlightAnimation.java
+++ ui/src/main/java/de/czoeller/depanalyzer/ui/animation/NodeFillHighlightAnimation.java
@@ -13,11 +13,16 @@ public class NodeFillHighlightAnimation<N, E> {
     private final VisualizationViewer<N, E> vv;
     private final List<N> nodes;
     private final Paint color;
+    /** static fields can't have type parameter */
+    private static Function backedUpNodeFillPaintFunction;
 
     public NodeFillHighlightAnimation(VisualizationViewer<N, E> vv, List<N> nodes, Paint color) {
         this.vv = vv;
         this.nodes = nodes;
         this.color = color;
+        if (backedUpNodeFillPaintFunction == null) {
+            backedUpNodeFillPaintFunction = vv.getRenderContext().getNodeFillPaintFunction();
+        }
         if(!nodes.isEmpty()) {
             java.util.Timer timer = new java.util.Timer();
             AnimationTimerTask at = new AnimationTimerTask(timer);
@@ -31,7 +36,7 @@ public class NodeFillHighlightAnimation<N, E> {
         private final double stepsize = 0.01;
         private final Timer timer;
         private double keyframe = 0 + width; // Between 0.0 and 1.0
-        private Function<? super N, Paint> backedUpNodeFillPaintFunction;
+
 
         public AnimationTimerTask(Timer timer) {
             this.timer = timer;
@@ -39,16 +44,12 @@ public class NodeFillHighlightAnimation<N, E> {
 
         @Override
         public void run() {
-            if (backedUpNodeFillPaintFunction == null) {
-                backedUpNodeFillPaintFunction = vv.getRenderContext().getNodeFillPaintFunction();
-            }
-
             vv.getRenderContext()
               .setNodeFillPaintFunction(n -> {
                   if (nodes.contains(n)) {
                       return color;
                   } else {
-                      return backedUpNodeFillPaintFunction.apply(n);
+                      return (Paint) backedUpNodeFillPaintFunction.apply(n);
                   }
               });
             keyframe += stepsize;
