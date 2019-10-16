2ae753042af7f910a1ccbef064a04ed921cdf300
Christian Zöller <noex_home@yahoo.de>
2019-10-16 18:49:30 +0200

    Fixed bug: last color on heatmap legend was not set as stop

--- ui/src/main/java/de/czoeller/depanalyzer/ui/components/help/HelpController.java
+++ ui/src/main/java/de/czoeller/depanalyzer/ui/components/help/HelpController.java
@@ -4,7 +4,6 @@ import de.czoeller.depanalyzer.ui.scorer.ScoreToHeatTransformer;
 import de.czoeller.depanalyzer.ui.util.GradientUtil;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Stop;
@@ -25,7 +24,7 @@ public class HelpController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         final java.awt.Color[] multiGradient = ScoreToHeatTransformer.multiGradient;
         final List<Stop> collect = IntStream.range(0, multiGradient.length)
-                                            .filter(i -> i % 25 == 0)
+                                            .filter(i -> i % 25 == 0 || i == multiGradient.length - 1)
                                             .mapToObj(i -> new Stop(i/100f, GradientUtil.fromAWTColor(multiGradient[i])))
                                             .collect(Collectors.toList());
         legendColorRect.setFill(new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE, collect));
